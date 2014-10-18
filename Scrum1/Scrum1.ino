cc699eb12e1c918c08ce961e0c61c183cae99464
Victoria Preston <victorialynnpreston@gmail.com>
2014-10-18 10:49:48 -0400

    Simple motor control with a pot for arm

---
+++ Scrum1/Scrum1.ino
@@ -0,0 +1,72 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+
+int pace;
+int i;
+unsigned long testing;
+float pot_value;
+int pot_pin = 0;
+int past_pot = 0;
+int velocity = 0;
+
+const int numReadings = 10;
+
+int readings[numReadings];      // the readings from the analog input
+int index = 0;                  // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+
+void setup() {
+  Serial.begin(9600);
+  AFMS.begin();
+  
+  pinMode(pot_pin, OUTPUT);
+  
+  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+    readings[thisReading] = 0;   
+  
+}
+
+void loop() {
+    // subtract the last reading:
+  total= total - readings[index];         
+  // read from the sensor:  
+  readings[index] = analogRead(pot_pin); 
+  // add the reading to the total:
+  total= total + readings[index];       
+  // advance to the next position in the array:  
+  index = index + 1;                    
+
+  // if we're at the end of the array...
+  if (index >= numReadings)              
+    // ...wrap around to the beginning: 
+    index = 0;                           
+
+  // calculate the average:
+  pot_value = total / numReadings;  
+
+  
+  if (pot_value > past_pot) {
+    myMotor->run(FORWARD);
+    velocity = pot_value*(255.0/1023.0);
+  }
+  else if (pot_value < past_pot) {
+    myMotor->run(BACKWARD);
+    velocity = abs(pot_value*(255.0/1023.0)-255.0);
+  }
+  else {
+    myMotor->run(RELEASE);
+  }
+ 
+  myMotor->setSpeed(velocity);
+  past_pot = pot_value;
+  
+  
+  delay(20);
+  
+}
