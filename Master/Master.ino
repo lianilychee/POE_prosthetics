d9b780435e4e434328e4155975c12c70fa7994b3
Victoria Preston <victorialynnpreston@gmail.com>
2014-11-05 14:55:39 -0500

    Adding meeting TODOs to master

--- Master/Master.ino
+++ Master/Master.ino
@@ -20,6 +20,8 @@ int finger_b = 2;
 int b;
 int finger_c = 3;
 int c;
+//TODO Accelerometer Data
+//TODO Dtate Booleans
 
 //Variables for smoothing
 const int numReadings = 10;
@@ -52,12 +54,25 @@ void setup()
     a_readings[thisReading] = 0;
     b_readings[thisReading] = 0;
     c_readings[thisReading] = 0;
+      
+  //TODO Relax the Actuators?
+  //TODO Calibrate the Accel?
 }
 
 /***********MAIN LOOP****************/
 void loop()
 {
-  
+  //TODO Add Controller If-Statement
+  /* Read in State
+  Determine Next State
+    if(relaxed) -> lift
+    if (lift) -> reach
+    if (reach, no hold) -> grab  
+    if (reach, held) -> release
+    if(grab) -> hold
+    if (not extended, held) -> lift (note: no actuation)
+    if (extended, held) -> release
+  Call next state*/
 }
 
 /***********FINGER SUBLOOP***********/
@@ -67,3 +82,32 @@ int fingerFeedback(signal){
 
 	//if not noise, send to smoothing
 }
+
+/*Actuation commands:
+relax() -> gripper (semi)
+
+
+lift()
+
+
+reach() -> gripper (open)
+	set finger servo: increment position to full open
+
+grab()
+set finger servo: increment position // see Actuator sketch for example
+
+// now start letting the person know that we’ve grabbed an object
+while pressure sensor reads some value
+	increment cuff servo
+	// got to do mapping of pressure sensor to cuff servo position
+
+// If we move the lower arm or if the servo is over torqued, exit
+if (angle shift || finger_pressure > threshold)
+exit function
+
+	
+	hold()
+		finger servo position = previous finger servo position
+		
+	release()
+		set finger servo: increment to open*/
