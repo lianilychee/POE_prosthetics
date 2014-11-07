2f2f9a11e54a7669603b378626a70852d88ae35a
Victoria Preston <victorialynnpreston@gmail.com>
2014-11-07 02:49:04 -0500

    Sketchy sketch version of finite state controller

--- Scrum2/Scrum2.ino
+++ Scrum2/Scrum2.ino
@@ -32,7 +32,6 @@ int bendAverage = 0;
 boolean relaxed = 0;
 boolean lift = 0;
 boolean reach = 0;
-boolean grab = 0;
 boolean hold = 0;
 boolean reelees = 0;
 
@@ -88,91 +87,109 @@ void loop()
   
   bend = map(bendAverage, 0, 1023, 0, 179);     // Probably also needs to be adjusted, scale it to use it with the servo (value between 0 and 180) 
   //gripServo.write(bend);                  // sets the servo position according to the scaled value 
-  
-  //delay(15);                           // waits for the servo to get there 
-  
+    
   if (relaxed) {
     relax(); 
+    Serial.println("relaxed");
   }
   else if (lift) {
-    lift();
+    arm_lift();
+    Serial.println("lift");
   }
   else if (reach && !hold) {
     grab();
+    Serial.println("grab");
   }
   else if (hold) {
-    hold();
+    hold_stuff();
+    Serial.println("hold");
   }
-  else if (reach && hold) {
-    reelees();
+  else if (reelees) {
+    reelees_now();
+    Serial.println("release");
   }
-  else if (reelees && reach){
-    relaxed = 1;
-  }
+
+  
+
 } 
 /****************RELAX LOOP*************/
 void relax() {
-  if (bendAverage < 5) {
+  if (bendAverage < 250) {
     relaxed = 1;
-    reach = 0;
     gripServo.write(90);
+    delay(15);                           // waits for the servo to get there 
   }
   else {
     relaxed = 0;
     lift = 1;
     gripServo.write(0);
+    delay(15);
   }
   cuffServo.write(0);
+  delay(15);
 }
 /**************LIFT LOOP***************/
-void lift() {
- if (bendAverage < 5) {
-  lift = 0;
-  reach = 1;
-  hold = 0;
+void arm_lift() {
+ if (bendAverage > 250) {
+  lift = 1;
+ }
+ else {
+   lift = 0;
+   reach = 1;
  }
   gripServo.write(0);
   cuffServo.write(0);
+  delay(15);
 }
 /***********GRAB LOOP****************/
 void grab() {
-  if (gripAverage < 300 && bendAverage < 5) {
-    hold = 0;
+  if (gripAverage < 300 && bendAverage < 250 && looper < 180) {
     reach = 1;
     cuffServo.write(grip);
     gripServo.write(looper);
-    looper = looper + 1;
+    looper = looper + 0.5;
     lastGrip = grip;
+    delay(15);
   }
   else {
     gripServo.write(looper);
     cuffServo.write(lastGrip);
     hold = 1;
     reach = 0;
+    delay(15);
   }
 }
 /**************HOLD LOOP*************/
-void hold() {
-  if (bendAverage < 5) {
-    reach = 1;
+void hold_stuff() {
+  if (bendAverage < 700) {
+    reelees = 0;
     hold = 1;
+  }
+  else {
+    reelees = 1;
+    hold = 0;
   }
 }
 /**************REELEES LOOP*********/
-void reelees() {
+void reelees_now() {
   if (looper >= 0) {
     gripServo.write(looper);
     cuffServo.write(lastGrip);
     lastGrip = grip;
     looper = looper - 1;
+    delay(15);
   }
   else {
+    relaxed = 1;
     hold = 0;
-    reach = 1;
+    reach = 0;
     lastGrip = 0;
     looper = 0;
+    reelees = 0;
     cuffServo.write(lastGrip);
     gripServo.write(looper);
+    Serial.println("reset");
+    delay(15);
   }
 }
     
